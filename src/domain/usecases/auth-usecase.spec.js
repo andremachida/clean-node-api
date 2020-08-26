1c641b531d049c2264f9402440a686476776ca96
Andre Machida <andmachida@gmail.com>
2020-08-25 21:04:10 -0300

    Created auth use cases;

---
+++ src/domain/usecases/auth-usecase.spec.js
@@ -0,0 +1,29 @@
+const { MissingParamError } = require('../../utils/errors')
+
+class AuthUseCase {
+  async auth (email, password) {
+    if (!email) {
+      throw new MissingParamError('email')
+    }
+
+    if (!password) {
+      throw new MissingParamError('password')
+    }
+  }
+}
+
+describe('Auth UseCase', () => {
+  test('Should throw error if no email is provided', async () => {
+    const sut = new AuthUseCase()
+    const promise = sut.auth()
+
+    expect(promise).rejects.toThrow(new MissingParamError('email'))
+  })
+
+  test('Should throw error if no password is provided', async () => {
+    const sut = new AuthUseCase()
+    const promise = sut.auth('any_email@email.com')
+
+    expect(promise).rejects.toThrow(new MissingParamError('password'))
+  })
+})
