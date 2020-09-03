1e6a03fc2b389d7c00b9603793849d9d3d1d449d
Andre Machida <andmachida@gmail.com>
2020-09-03 20:41:29 -0300

    Created token generator tests;

---
+++ src/utils/helpers/token-generator.spec.js
@@ -0,0 +1,47 @@
+const jwt = require('jsonwebtoken')
+const TokenGenerator = require('./token-generator')
+
+const MissingParamError = require('../errors/missing-param-error')
+
+const makeSut = () => {
+  return new TokenGenerator('secret')
+}
+
+describe('TokenGenerator', () => {
+  test('Should return null if JWT returns null', async () => {
+    const sut = makeSut()
+    jwt.token = null
+    const token = await sut.generate('any_id')
+
+    expect(token).toBeNull()
+  })
+
+  test('Should return a token if JWT return token', async () => {
+    const sut = makeSut()
+    const token = await sut.generate('any_id')
+
+    expect(token).toBe(jwt.token)
+  })
+
+  test('Should call JWt with correct values', async () => {
+    const sut = makeSut()
+    await sut.generate('any_id')
+
+    expect(jwt.id).toBe('any_id')
+    expect(jwt.secret).toBe(sut.secret)
+  })
+
+  test('Should throw an exception if no secret provided', async () => {
+    const sut = new TokenGenerator()
+    const promise = sut.generate('any_id')
+
+    expect(promise).rejects.toThrow(new MissingParamError('secret'))
+  })
+
+  test('Should throw an exception if no id provided', async () => {
+    const sut = makeSut()
+    const promise = sut.generate()
+
+    expect(promise).rejects.toThrow(new MissingParamError('id'))
+  })
+})
